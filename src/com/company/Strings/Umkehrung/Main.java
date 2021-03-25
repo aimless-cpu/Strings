84f27f4d1d9dad6a0d86f0b6c08dbfe2ccbb161a
aimless-cpu <s.grabher@protonmail.com>
2021-03-25 19:05:51 +0100

    Umkehrung main

---
+++ src/com/company/Strings/Umkehrung/Main.java
@@ -0,0 +1,13 @@
+package com.company.Strings.Umkehrung;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String input = "katze";
+
+        StringBuilder sb = new StringBuilder(input);
+        System.out.println(sb.reverse());
+    }
+
+}
